a7cdc5b9955b9b61706606b755efc41708726929
YaserAp <125145424+YaserAp@users.noreply.github.com>
2025-08-29 00:46:19 +0700

    Update YSUI.lua

--- YSUI.lua
+++ YSUI.lua
@@ -391,7 +391,7 @@ function YSSHLibrary:CreateWindow(settings)
 
   local window = {}
 
-  local MAIN_WIDTH = settings.Width or 520
+  local MAIN_WIDTH = settings.Width or 540
   local MAIN_HEIGHT = settings.Height or 315
 
   local main = make("Frame", {
@@ -1539,5 +1539,6 @@ end
 ----------------------------------------------------------------
 
 return YSSHLibrary
+
 
 
